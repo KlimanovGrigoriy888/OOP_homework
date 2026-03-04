6e56987b7dbef9e97f103012115a52bba341d75c
Grigoriy Klimanov <5g11s198088@gmail.com>
2026-03-04 21:32:29 +0300

    fixes tests files for test

--- tests/test_category.py
+++ tests/test_category.py
@@ -1,22 +1,37 @@
+from src.сategory import Category
+from src.product import Product
+
+
 def test_category_second_category(category_first_category, category_second_category):
     assert category_first_category.name == "Смартфоны"
     assert category_second_category.name == "Телевизоры"
     assert (
-        category_first_category.description
-        == "Смартфоны, как средство не только коммуникации, но и получения дополнительных функций для удобства жизни"
+            category_first_category.description
+            == "Смартфоны, как средство не только коммуникации, но и получения дополнительных функций для удобства жизни"
     )
     assert (
-        category_second_category.description
-        == "Современный телевизор, который позволяет наслаждаться просмотром, станет вашим другом и помощником"
+            category_second_category.description
+            == "Современный телевизор, который позволяет наслаждаться просмотром, станет вашим другом и помощником"
     )
-    assert len(category_first_category.products) == 2
+    assert category_first_category.products == ("Samsung Galaxy S23 Ultra, 180000.0 руб. Остаток: 5 шт."
+                                                "\nIphone 15, 210000.0 руб. Остаток: 8 шт.\n")
     assert category_first_category.category_count == 2
     assert category_first_category.product_count == 3
     assert category_second_category.name == "Телевизоры"
     assert (
-        category_second_category.description
-        == "Современный телевизор, который позволяет наслаждаться просмотром, станет вашим другом и помощником"
+            category_second_category.description
+            == "Современный телевизор, который позволяет наслаждаться просмотром, станет вашим другом и помощником"
     )
-    assert len(category_second_category.products) == 1
+    assert category_second_category.products == '55" QLED 4K, 123000.0 руб. Остаток: 7 шт.\n'
     assert category_second_category.category_count == 2
     assert category_second_category.product_count == 3
+    product2 = Product("32\" QLED 4K", "Фоновая подсветка", 60000.0, 2)
+    category_second_category.add_product(product2)
+    assert category_second_category.products == '55" QLED 4K, 123000.0 руб. Остаток: 7 шт.\n32" QLED 4K, 60000.0 руб. Остаток: 2 шт.\n'
+    assert category_second_category.name == "Телевизоры"
+    assert category_second_category.description == "Современный телевизор, который позволяет наслаждаться просмотром, станет вашим другом и помощником"
+    assert Category.category_count == 2
+    assert Category.product_count == 4
+
+
+
--- tests/test_product.py
+++ tests/test_product.py
@@ -1,5 +1,51 @@
-def test_first_product(products_first):
-    assert products_first.name == "Samsung Galaxy S23 Ultra"
-    assert products_first.description == "256GB, Серый цвет, 200MP камера"
-    assert products_first.quantity == 5
-    assert products_first.price == 180000.0
+from src.product import Product
+from unittest.mock import patch
+
+
+def test_product():
+    # Очищаем словарь, чтобы тесты были независимыми
+    Product.added_name_products = {}
+
+    # Тест для проверки добавления нового продукта product_0
+    product_0 = Product.new_product({
+        "name": "Samsung Galaxy S23 Ultra",
+        "description": "256GB, Серый цвет, 200MP камера",
+        "price": 18000.0,
+        "quantity": 5
+    })
+    assert product_0.quantity == 5
+    assert product_0.price == 18000.0
+
+    # Тест для проверки добавления нового продукта product_1
+    product_1 = Product.new_product({
+        "name": "Samsung Galaxy S23 Ultra",
+        "description": "256GB, Серый цвет, 200MP камера",
+        "price": 18000.0,
+        "quantity": 5
+    })
+    assert product_1.quantity == 5
+    assert product_1.price == 18000.0
+
+    # Проверка состояния атрибута класса в виде словаря который хранит данные имени и цене товаров.
+    assert Product.added_name_products == {'Samsung Galaxy S23 Ultra': [10, 18000.0]}
+
+    # Тест сеттера
+
+    # 1. Повышение цены (проходит без input)
+    product_1.price = 200000.0
+    assert product_1.price == 200000.0
+
+    # 2. Снижение цены — СОГЛАСИЕ (имитируем ввод 'y')
+    with patch('builtins.input', return_value='y'):
+        product_1.price = 150000.0
+        assert product_1.price == 150000.0
+
+    # 3. Снижение цены — ОТКАЗ (имитируем ввод 'n')
+    with patch('builtins.input', return_value='n'):
+        product_1.price = 100000.0
+        # Цена должна остаться 150000.0, так как мы нажали 'n'
+        assert product_1.price == 150000.0
+
+    # 4. Некорректная цена
+    product_1.price = -100
+    assert product_1.price == 150000.0
