da51ae658707d53d29977afcb698f265a19713de
Grigoriy Klimanov <5g11s198088@gmail.com>
2026-03-12 23:38:17 +0300

    fixes tests for new changes

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,6 +1,8 @@
 import pytest
 
+from src.lawngrass import LawnGrass
 from src.product import Product
+from src.smartphone import Smartphone
 from src.сategory import Category
 
 
@@ -23,3 +25,29 @@ def category_second_category():
         "Современный телевизор, который позволяет наслаждаться просмотром, станет вашим другом и помощником",
         [Product('55" QLED 4K', "Фоновая подсветка", 123000.0, 7)],
     )
+
+
+@pytest.fixture
+def smartphone_new_1():
+    return Smartphone("Samsung Galaxy S23 Ultra", "256GB, Серый цвет, 200MP камера", 180000.0, 5, 95.5,
+                      "S23 Ultra", 256, "Серый")
+
+
+@pytest.fixture
+def smartphone_new_2():
+    return Smartphone("Iphone 15", "512GB, Gray space", 210000.0, 8, 98.2, "15", 512, "Gray space")
+
+
+@pytest.fixture
+def smartphone_new_3():
+    return Smartphone("Xiaomi Redmi Note 11", "1024GB, Синий", 31000.0, 14, 90.3, "Note 11", 1024, "Синий")
+
+
+@pytest.fixture
+def lawngrass_new_1():
+    return LawnGrass("Газонная трава", "Элитная трава для газона", 500.0, 20, "Россия", "7 дней", "Зеленый")
+
+
+@pytest.fixture
+def lawngrass_new_2():
+    return LawnGrass("Газонная трава 2", "Выносливая трава", 450.0, 15, "США", "5 дней", "Темно-зеленый")
--- tests/test_products_iterator.py
+++ tests/test_products_iterator.py
@@ -1,5 +1,6 @@
 from src.products_iterator import ProductIterator
 
+
 # Тест для класса ProductIterator, проверка продуктов в экземпляре класса.
 def test_products_iterator(category_first_category):
     products = [
@@ -8,6 +9,6 @@ def test_products_iterator(category_first_category):
     ]
     product_list = ProductIterator(category_first_category)
     for i, product in enumerate(product_list):
+        # print(products[i])
         assert str(product) == products[i]
         assert str(product) == products[i]
-
